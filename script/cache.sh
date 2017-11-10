52aba78d0b08a8a79ba2c807a9ea2697b72fd1ef
Costin Manolache <costin@google.com>
2017-11-10 13:07:53 -0800

    Attempt to cache the dir

--- script/cache.sh
+++ script/cache.sh
@@ -1,8 +1,11 @@
 #!/usr/bin/env bash
 
+PROJECT_ID=${PROJECT_ID:-istio-costin}
+
 if [ "$1" == "save" ]; then
   tar cfz bazel_cache.tgz ${HOME}/.cache/bazel
 elif [ "$1" == "restore" ]; then
+  gsutil cp gs://$PROJECT_ID/proxy_cache bazel_cache.tgz
   if [ -f bazel_cache.tgz ]; then
     tar xfz bazel_cache.tgz ${HOME}/.cache/bazel
   fi
