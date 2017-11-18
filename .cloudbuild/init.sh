da679269435123cab69809018ee9c394480897d5
Costin Manolache <costin@gmail.com>
2017-11-17 17:05:27 -0800

    Another attempt for cloudbuild

--- .cloudbuild/init.sh
+++ .cloudbuild/init.sh
@@ -5,5 +5,10 @@ ls -l /workspace
 ls -l /builder
 ls -l /builder/home
 
+export USER=circleci
+export HOME=/workspace
 sudo chown -R $USER /workspace
 sudo chown -R $USER /builder/home
+
+make build_envoy
+
